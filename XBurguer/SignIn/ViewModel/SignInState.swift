f5fdda2f20d941d15ec7687c3f38d2d4b5f55227
Patrick <patrickalvesarts@gmail.com>
2024-08-04 17:23:36 -0300

    nagigation Controller

---
+++ XBurguer/SignIn/ViewModel/SignInState.swift
@@ -0,0 +1,8 @@
+//
+//  SignInState.swift
+//  XBurguer
+//
+//  Created by Patrick Alves on 4/8/24.
+//
+
+import Foundation
---
+++ XBurguer/SignIn/ViewModel/SignInViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SignInViewModel.swift
+//  XBurguer
+//
+//  Created by Patrick Alves on 2/8/24.
+//
+
+import Foundation
